41229171e92abd719d6c2702cee96cd1d41869d4
Joao Micena (JP) <joao.micena@nubank.com.br>
2020-08-06 13:44:35 -0300

    Introduction to concurrent programming

--- src/lectures/part3cp/Intro.scala
+++ src/lectures/part3cp/Intro.scala
@@ -54,4 +54,100 @@ object Intro extends App {
   println(pool.isShutdown)
   println(pool.isTerminated)
 
+  def runInParallel = {
+    var x = 0
+
+    val thread1 = new Thread(() => {
+      x = 1
+    })
+
+    val thread2 = new Thread(() => {
+      x = 2
+    })
+
+    thread1.start()
+    thread2.start()
+    println(x)
+  }
+
+  for (_ <- 1 to 100) runInParallel
+  // race condition (two threads trying to access the same memory zone at the same time)
+
+  class BankAccount(var amount: Int) {
+    override def toString: String = "" + amount
+  }
+  def buy(account: BankAccount, thing: String, price: Int) = {
+    account.amount -= price
+    //println("I've bought " + thing)
+    //println("my account is now " + account)
+  }
+
+  for (_ <- 1 to 1000) {
+    val account = new BankAccount(50000)
+    val thread1 = new Thread(() => buy(account, "shoes", 3000))
+    val thread2 = new Thread(() => buy(account, "iPhone12", 4000))
+
+    thread1.start()
+    thread2.start()
+    Thread.sleep(10)
+    if (account.amount != 43000) println("AHA: " + account.amount)
+  }
+
+  // option #1: use synchronized()
+  def buySafe(account: BankAccount, thing: String, price: Int) =
+    account.synchronized {
+      // no two threads can evaluate this at the same time
+      account.amount -= price
+      println("I've bought " + thing)
+      println("my account is now " + account)
+    }
+
+  // option #2: use @volatile (annotating the var: @volatile var amount: Int
+
+  /*
+    Exercises
+
+    1) Construct 50 "inception" threads
+        Thread1 -> thread2 -> thread3
+        println("hello from thread X")
+       in REVERSE ORDER
+
+    2) Code below, answer questions:
+       - what is the biggest value possible for x? 100
+       - what is the smallest value possible for x? 1
+
+    3) sleep fallacy
+      - what is the value of the message? almost always "scala is awesome"
+      - is it guaranteed? No!
+   */
+
+  // Exercise 1
+  def inceptionThreads(maxThreads: Int, i: Int): Thread = new Thread(() => {
+    if (i < maxThreads) {
+      val newThread = inceptionThreads(maxThreads, i + 1)
+      newThread.start()
+      newThread.join()
+    }
+    println(s"hello from thread $i")
+  })
+
+  inceptionThreads(50, 1).start()
+
+  // Exercise 2
+  var x = 0
+  val threads = (1 to 100).map(_ => new Thread(() => x += 1))
+  threads.foreach(_.start)
+
+  // Exercise 3
+  var message = ""
+  val awesomeThread = new Thread(() => {
+    Thread.sleep(1000)
+    message = "scala is awesome"
+  })
+
+  message = "scala sux"
+  awesomeThread.start()
+  Thread.sleep(2000)
+  println(message)
+
 }
